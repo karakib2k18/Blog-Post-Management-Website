15b19bdf05e081115f66b06848936dc485a50068
KAZI ABDUR RAKIB <rakib@pop-os.localdomain>
2025-05-07 04:50:20 +0800

    bug fix

--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  tags: [String], // Example: ['nodejs', 'express']
+  tags: [String],
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   createdAt: { type: Date, default: Date.now }
 });
